a532c12aba49bb83a08f60ebbaabb2e21dab58ed
Amisha Pandey <amisha.pandey2006@gmail.com>
2026-07-05 01:38:45 +0530

    Add rate limiting to part routes

--- server/route/partRoutes.js
+++ server/route/partRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import upload from "../middleware/multer.js";
+import rateLimit from "express-rate-limit";
+import { upload, validateImageSignature } from "../middleware/multer.js";
 import {
   addPart,
   getAllParts,
@@ -12,20 +13,18 @@ import {
 } from "../controllers/partsControllers.js";
 import auth from "../middleware/auth.js";
 import admin from "../middleware/Admin.js";
-
+const adminLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per window
+  message: "Too many requests from this IP, please try again later."
+});
 const partRouter = express.Router();
 
-partRouter.post("/add", upload.array("images", 5), auth, admin, addPart);
+partRouter.post("/add", adminLimiter, upload.array("images", 5), validateImageSignature, auth, admin, addPart);
 partRouter.get("/get", getAllParts);
 partRouter.get("/get/:id", getPartById);
 partRouter.get("/get/:id/similar", getSimilarParts);
-partRouter.put(
-  "/update/:id",
-  upload.array("images", 5),
-  auth,
-  admin,
-  updatePart
-);
+partRouter.put("/update/:id", adminLimiter, upload.array("images", 5), validateImageSignature, auth, admin, updatePart);
 partRouter.delete("/delete/:id", auth, admin, deletePart);
 partRouter.post("/review/:id", auth, createOrUpdateReview);
 partRouter.delete("/review/:id", auth, deleteReview);
